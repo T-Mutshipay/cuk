02b71b01a106117b8f65ee0b25110e2f98708801
T-Mutshipay <heritiermutshipay@gmail.com>
2025-01-22 07:17:32 +0100

    Renommer « examens_table » en « examens » lors de la migration pour plus de clarté et de cohérence

--- database/migrations/2025_01_13_143058_create_examens_tbale.php
+++ database/migrations/2025_01_13_143058_create_examens_tbale.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('examens_tbale', function (Blueprint $table) {
+        Schema::create('examens', function (Blueprint $table) {
             $table->id();
             $table->foreignId('patient_id')->constrained()->cascadeOnDelete();
             $table->string('type');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('examens_tbale');
+        Schema::dropIfExists('examens');
     }
 };
